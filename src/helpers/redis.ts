97ded45afe27d7fe466745ca7454348fccd5eb85
Shuvam Santra <76802373+GamerQuanTuM@users.noreply.github.com>
2025-05-20 20:26:37 +0530

    Update redis.ts
    Removed the hardcorded commented redis credentials

--- src/helpers/redis.ts
+++ src/helpers/redis.ts
@@ -2,9 +2,6 @@ import env from "@/env";
 import Redis from "ioredis";
 
 const redis = new Redis({
-    // host: "redis-14717.crce179.ap-south-1-1.ec2.redns.redis-cloud.com",
-    // port: 14717,
-    // password: "rblxTbocku6oDCEsPkwtmTFtES6OoPSc",
     host: env.REDIS_HOST,
     port: env.REDIS_PORT,
     password: env.REDIS_PASSWORD,
